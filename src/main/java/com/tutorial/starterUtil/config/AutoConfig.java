b509c0d00a540569a45bde1de291402e0dca1a10
sivaprakash <sivaprakash@zeemart.asia>
2019-08-01 16:21:48 +0530

    removed unwanted

--- src/main/java/com/tutorial/starterUtil/config/AutoConfig.java
+++ src/main/java/com/tutorial/starterUtil/config/AutoConfig.java
@@ -1,6 +1,5 @@
 package com.tutorial.starterUtil.config;
 
-
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -13,25 +12,10 @@ import com.tutorial.starterUtil.service.TestSampleImpl;
 @ConditionalOnClass(TestSample.class)
 public class AutoConfig {
 
-  /*  @Autowired
-    private Properties greeterProperties;
- 
-    @Bean
-    @ConditionalOnMissingBean
-    public void greeterConfig() {
- 
-        String userName = greeterProperties.getUrl() == null
-          ? System.getProperty("test.prop.url") 
-          : greeterProperties.getUrl();
-System.out.println(userName);         
-    }*/
-    
     @Bean
     @ConditionalOnMissingBean
     public TestSample testSample() {
         return new TestSampleImpl();
     }
-    
-   
-   
+
 }
